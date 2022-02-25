7fadfb5525eae3c4e85529c0d10f750913f1c919
Ubuntu <smo@leibniz-hbi.de>
2022-02-25 16:35:55 +0000

    [add] latest data

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,6 @@
 cd /home/ubuntu/ukraine_data
 pipenv run python scripts/refresh_files.py
-git add *.csv
-git add *.sh
+git add --ignore-removal *.csv
 git commit -a -m "[add] latest data"
 git push git@github.com-ukraine_data:Leibniz-HBI/ukraine_data
 
