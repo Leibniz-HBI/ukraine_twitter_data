9d218c02f64128c01a0d35c2aa6173ba47a37c9d
Ubuntu <smo@leibniz-hbi.de>
2022-02-25 19:48:22 +0000

    [fix] pull before update

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 cd /home/ubuntu/ukraine_data
+git pull
 pipenv run python scripts/refresh_files.py
-git add *.csv
-git add *.sh
-git commit -a -m "[add] latest data"
+git add --ignore-removal *.csv
+git commit -m "[add] latest data"
 git push git@github.com-ukraine_data:Leibniz-HBI/ukraine_data
 
