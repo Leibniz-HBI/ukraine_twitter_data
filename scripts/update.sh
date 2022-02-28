e2b73d002a8e794c51bdfa872f4bbc37a23b65f6
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-02-28 17:01:40 +0100

    [update] script for timestamped commit messages

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,6 @@ cd /home/ubuntu/ukraine_data
 git pull
 pipenv run python scripts/refresh_files.py
 git add --ignore-removal *.csv
-git commit -m "[add] latest data"
+git commit -m "[add] data for $(date +%F)"
 git push git@github.com-ukraine_data:Leibniz-HBI/ukraine_data
 
