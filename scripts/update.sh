e17f89bdf287815f8a352eb34be33d9e54011ec5
Philipp Kessling <p.kessling@leibniz-hbi.de>
2022-03-03 14:53:52 +0100

    [remove] manual file splitting

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 cd /home/ubuntu/ukraine_data
 git pull
 pipenv run python scripts/refresh_files.py
-pipenv run python scripts/split_files.py
+# pipenv run python scripts/split_files.py
 git add --ignore-removal *.csv
 git commit -m "[add] data for $(date +%F)"
 git push git@github.com-ukraine_data:Leibniz-HBI/ukraine_data
