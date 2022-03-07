bfd82b37dc42647dd95840fe7d9dc4115e14bb0a
Ubuntu <smo@leibniz-hbi.de>
2022-03-07 17:21:44 +0000

    remove English data

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,6 +1,6 @@
 cd /home/ubuntu/ukraine_data
 git pull
-pipenv run python scripts/refresh_files.py
+pipenv run python scripts/refresh_files.py --chunk_size 250000
 # pipenv run python scripts/split_files.py
 git add --ignore-removal *.csv
 git commit -m "[add] data for $(date +%F)"
