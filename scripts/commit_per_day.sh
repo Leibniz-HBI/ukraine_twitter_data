f80e7beb1222885a6d8ff76c9bcf71bc50dce1ee
Ubuntu <smo@leibniz-hbi.de>
2022-04-13 17:18:23 +0000

    edit config

--- scripts/commit_per_day.sh
+++ scripts/commit_per_day.sh
@@ -1,24 +1,24 @@
-for i in {01..28}
+for i in {01..31}
 do	
-	git add *hydrator-2022-02-"$i"*.csv
+	git add *hydrator-2022-03-"$i"*.csv
 
 	for j in {0..6}
 	do
-		git add *2022-02-"$i"*_"$j".csv
-		git commit -m "update 2022-02-$i $j"
+		git add *2022-03-"$i"*_"$j".csv
+		git commit -m "update 2022-03-$i bucha-en"
 		git push git@github.com-ukraine_data:Leibniz-HBI/ukraine_data
 	done
 done
-for i in {01..10}
+for i in {01..12}
 do
-	        git add *hydrator-2022-03-"$i"*.csv
+        git add *hydrator-2022-04-"$i"*.csv
 
 	for j in {0..6}
 	do
-	       git add *2022-03-"$i"*_"$j".csv
-	       git commit -m "update 2022-03-$i $j"
+	       git add *2022-04-"$i"*_"$j".csv
+	       git commit -m "update 2022-04-$i bucha-en"
  	       git push git@github.com-ukraine_data:Leibniz-HBI/ukraine_data
-        done
+    done
 done
 
 
